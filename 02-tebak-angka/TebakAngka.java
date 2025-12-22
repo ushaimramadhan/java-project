547e3a74fff6682326ca7c1c4580f51f8054381b
ushaim <adhan1005@gmail.com>
2025-12-22 09:59:56 +0700

    Update projek tebak angka: refactor code

--- 02-tebak-angka/TebakAngka.java
+++ 02-tebak-angka/TebakAngka.java
@@ -2,33 +2,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TebakAngka {
+    private static final int maxRange = 100;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         System.out.println("=== GAME TEBAK ANGKA ===");
-        System.out.println("Saya sudah memilih angka antara 1 sampai 100.");
+        System.out.println("Saya sudah memilih angka antara 1 sampai " + maxRange);
         System.out.println("Coba tebak ya!");
 
-        int targetAngka = random.nextInt(100) + 1;
-        int tebakanUser = 0;
+        int targetAngka = random.nextInt(maxRange) + 1;
+        int tebakanUser;
         int jumlahPercobaan = 0;
 
-        while(tebakanUser != targetAngka) {
-            System.out.print("\nMasukkan tebakanmu: ");
+        do {
+            System.out.print("Masukkan tebakanmu: ");
             tebakanUser = scanner.nextInt();
             jumlahPercobaan++;
 
             if (tebakanUser == targetAngka) {
-                System.out.println("Selamat kamu berhasil menebak angkanya!");
-                System.out.printf("Jumlah percobaan: %d \n", jumlahPercobaan);
+                System.out.println("Selamat tebakanmu benar!");
+                System.out.println("Total percobaan: " + jumlahPercobaan);
             } else if (tebakanUser < targetAngka) {
                 System.out.println("Terlalu kecil! coba lagi.");
             } else {
                 System.out.println("Terlalu besar! coba lagi.");
             }
-        }
+        } while (tebakanUser != targetAngka);
+
         scanner.close();
     }
 }

530f6ca12c755c3c81163795117a53329606db4f
ushaim <adhan1005@gmail.com>
2025-12-25 06:21:45 +0700

    feat: create subclass FullTimeEmployee and Intern

--- 04-sistem-payroll-karyawan/SistemPayrollKaryawan.java
+++ 04-sistem-payroll-karyawan/SistemPayrollKaryawan.java
@@ -26,3 +26,45 @@ abstract class Employee {
         System.out.println("Slip Gaji " + name + " (" + id + ")");
     }
 }
+
+class FullTimeEmployee extends Employee implements Taxable {
+    private double baseSalary;
+    private double bonus;
+    
+    public FullTimeEmployee(String id, String name, double baseSalary, double bonus) {
+        super(id, name);
+        this.baseSalary = baseSalary;
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return baseSalary + bonus;
+    }
+
+    @Override
+    public double getTaxRate() {
+        return 0.10;
+    }
+
+    @Override
+    public double calculateTax(double grossSalary) {
+        return grossSalary * getTaxRate();
+    }
+}
+
+class Intern extends Employee {
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public Intern(String id, String name, int hoursWorked, double hourlyRate){
+        super(id, name);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+}

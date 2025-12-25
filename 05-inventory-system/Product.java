03a7684610a428db59757ec7bbb079b74d781212
ushaim <adhan1005@gmail.com>
2025-12-25 10:52:23 +0700

    feat: Create Class Product (blueprint)

---
+++ 05-inventory-system/Product.java
@@ -0,0 +1,34 @@
+public class Product {
+    private String id;
+    private String name;
+    private double price;
+    private int stock;
+
+    public Product(String id, String name, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int ubah) {
+        stock = ubah;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + this.id + "Name: " + this.name + "Price: " + this.price + "Stock: " + this.stock;
+    }
+}

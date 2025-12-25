5ab1cedc37796329b3d7033e1fc1085c7984f6ee
ushaim <adhan1005@gmail.com>
2025-12-25 17:11:05 +0700

    refactor: display csv to filter it only display food

--- 06-csv-transaction-analyzer/CsvTransactionAnalyzer.java
+++ 06-csv-transaction-analyzer/CsvTransactionAnalyzer.java
@@ -14,15 +14,18 @@ public class CsvTransactionAnalyzer {
         String fileName = "transaction.csv";
 
         try {
-            List<Transaction> transactions = Files.lines(Paths.get(fileName))
+            int totalFood = Files.lines(Paths.get(fileName))
                 .skip(1)
                 .map(line -> {
                     String[] parts = line.split(",");
                     return new Transaction(parts[0], parts[1], Integer.parseInt(parts[2]), parts[3]);
                 })
-                .collect(Collectors.toList());
+                .filter(t -> t.category().equalsIgnoreCase("Food"))
+                .mapToInt(t -> t.amount())
+                .sum();
 
-            transactions.forEach(System.out::println);
+                System.out.println("Total pengeluaran Makanan: Rp " + totalFood);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
